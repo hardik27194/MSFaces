d4814708a0e94174ff90d20b7e80d90331f01110
Mingyang Li <mingyangli511@gmail.com>
2016-10-03 14:08:21 +0900

    Set up Xcode project

---
+++ MSFaces/MSFaces/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MSFaces
+//
+//  Created by Lee on 10/3/16.
+//  Copyright © 2016 Microsoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MSFaces/MSFaces/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MSFaces
+//
+//  Created by Lee on 10/3/16.
+//  Copyright © 2016 Microsoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
